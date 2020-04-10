08269bfb7a70cb4b35d94664464d34ab491f3dc8
Sudhir Yadav <sudhir22yadav@gmail.com>
2020-04-10 00:59:52 -0600

    auto generated files by dataFormat and other python files

--- scrapping.py
+++ scrapping.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
+import random
+
 from selenium.webdriver.common import keys
 
-
+'''
 browser = webdriver.Chrome()
 browser.get('https://www.websudoku.com/?level=1')
 browser.find_elements_by_tag_name('tbody')
@@ -12,16 +15,20 @@ browser.find_elements_by_tag_name('tbody')
 browser.find_elements_by_xpath('//*[@id=\"puzzle_grid\"]')
 
 
-
-
 '''
-url = 'https://www.nine.websudoku.com/?level=1&set_id=2592005782'
+run = True
+
+ls = []
+# time.sleep(random.randint(2,10))
+url = 'https://nine.websudoku.com/'
 
 header = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36\
-        (KHTML, like Gecko) Ubuntu Chromium/80.0.3987.87 Chrome/80.0.3987.87 Safari/537.36'}
+                        (KHTML, like Gecko) Ubuntu Chromium/80.0.3987.87 Chrome/80.0.3987.87 Safari/537.36'}
 
-page = requests.get(url, headers= header)
-
+page = requests.get(url, headers=header)
+print(page)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify())
-'''
+elements = soup.find(attrs={'id': 'cheat'})
+#sol.writelines(elements['value'])
+print(elements['value'])
